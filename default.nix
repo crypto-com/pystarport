249e34bee55b54e675fa7412f8f3ea95fc2a7741
huangyi <huang@crypto.com>
2021-05-30 12:46:04 +0800

    remove default.nix

--- default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs ? import <nixpkgs> { }
-, chaind ? "chain-maind"
-}:
-pkgs.poetry2nix.mkPoetryApplication {
-  projectDir = ./.;
-  preBuild = ''
-    sed -i -e 's@CHAIN = ""  # edit by nix-build@CHAIN = "${chaind}"@' pystarport/app.py
-  '';
-}
