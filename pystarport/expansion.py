19fe003d2ee6e4309862d1cf5ab9fff24aefa45b
Damon Chen <damon.chen@crypto.com>
2021-12-02 12:19:08 +0800

    Remove _str_to_python_value and remove {}

--- pystarport/expansion.py
+++ pystarport/expansion.py
@@ -1,5 +1,4 @@
 import os
-import re
 from pathlib import Path
 from typing import Any, Mapping, Optional, Text
 
@@ -22,11 +21,11 @@ def expand_posix_vars(obj: Any, variables: Mapping[Text, Optional[Any]]) -> Any:
         for index in range(len(obj)):
             obj[index] = expand_posix_vars(obj[index], variables)
     elif isinstance(obj, (str,)):
-        obj = _str_to_python_value(_expand(obj, variables))
+        obj = _expand(obj, variables)
     return obj
 
 
-def _expand(value, variables={}):
+def _expand(value, variables):
     """_expand does POSIX-style variable expansion
 
     This is adapted from python-dotenv, specifically here:
@@ -41,25 +40,6 @@ def _expand(value, variables={}):
         return value
     atoms = parse_variables(value)
     return "".join([str(atom.resolve(variables)) for atom in atoms])
-
-
-INT_REGEX = re.compile(r"^[-+]?[0-9]+$")
-
-
-def _str_to_python_value(val):
-    """_str_to_python_value infers the data type from a string.
-
-    This could eventually use PyYAML's parsing logic.
-    """
-    if not isinstance(val, (str,)):
-        return val
-    elif val == "true" or val == "True" or val == "on":
-        return True
-    elif val == "false" or val == "False" or val == "off":
-        return False
-    # elif INT_REGEX.match(val):
-    #     return int(val)
-    return val
 
 
 def expand_yaml(config_path, dotenv):
