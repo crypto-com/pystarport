67ae84773512d122d24870be73b9a4baae063f02
mmsqe <mavis@crypto.com>
2022-06-18 19:06:54 +0800

    flake update with poetry2nix fix
    for more info, see https://github.com/nix-community/poetry2nix/pull/670

--- flake.nix
+++ flake.nix
@@ -2,39 +2,15 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/22.05";
     flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix = {
-      url = "github:nix-community/poetry2nix";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
+  outputs = { self, nixpkgs, flake-utils }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs {
-          inherit system; config = { };
-          overlays = [
-            poetry2nix.overlay
-          ];
-        });
+        pkgs = (import nixpkgs { inherit system; config = { }; });
       in
       rec {
-        defaultPackage = pkgs.poetry2nix.mkPoetryApplication
-          {
+        defaultPackage = pkgs.poetry2nix.mkPoetryApplication {
             projectDir = ./.;
-            overrides = pkgs.poetry2nix.overrides.withDefaults
-              (self: super: {
-                pyparsing = super.pyparsing.overridePythonAttrs (
-                  old: {
-                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
-                  }
-                );
-                jsonschema = super.jsonschema.overridePythonAttrs (
-                  old: {
-                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.hatchling self.hatch-vcs ];
-                  }
-                );
-              });
           };
         defaultApp = {
           type = "app";
