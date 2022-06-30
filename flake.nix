45815cf3fad506345d196035c7c5a2da1a944503
HuangYi <huang@crypto.com>
2022-06-30 11:54:36 +0800

    add back overrides

--- flake.nix
+++ flake.nix
@@ -9,9 +9,23 @@
         pkgs = (import nixpkgs { inherit system; config = { }; });
       in
       rec {
-        packages.default = pkgs.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-        };
+        packages.default = pkgs.poetry2nix.mkPoetryApplication
+          {
+            projectDir = ./.;
+            overrides = pkgs.poetry2nix.overrides.withDefaults
+              (self: super: {
+                pyparsing = super.pyparsing.overridePythonAttrs (
+                  old: {
+                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
+                  }
+                );
+                jsonschema = super.jsonschema.overridePythonAttrs (
+                  old: {
+                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.hatchling self.hatch-vcs ];
+                  }
+                );
+              });
+          };
         apps.default = {
           type = "app";
           program = "${packages.default}/bin/pystarport";
