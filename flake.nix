b53341f74f754a152df999c1a74109f28deccfcf
yihuang <huang@crypto.com>
2023-04-21 18:59:47 +0800

    Problem: flake.nix is out dated (#85)
    * Problem: flake.nix is out dated
    
    Solution:
    - update nixpkgs to 22.11 release
    - fix poetry2nix overrides
    
    * poetry update pathspec
    
    ---------
    
    Co-authored-by: mmsqe <mavis@crypto.com>

--- flake.nix
+++ flake.nix
@@ -1,30 +1,51 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/22.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = { self, nixpkgs, flake-utils }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = (import nixpkgs { inherit system; config = { }; });
+        overrides = pkgs.poetry2nix.overrides.withDefaults (pkgs.lib.composeManyExtensions [
+          (self: super:
+            let
+              buildSystems = {
+                eth-bloom = [ "setuptools" ];
+                cprotobuf = [ "setuptools" ];
+                durations = [ "setuptools" ];
+                multitail2 = [ "setuptools" ];
+                pytest-github-actions-annotate-failures = [ "setuptools" ];
+                flake8-black = [ "setuptools" ];
+                multiaddr = [ "setuptools" ];
+              };
+            in
+            pkgs.lib.mapAttrs
+              (attr: systems: super.${attr}.overridePythonAttrs
+                (old: {
+                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
+                }))
+              buildSystems
+          )
+          (self: super: {
+            eth-bloom = super.eth-bloom.overridePythonAttrs {
+              preConfigure = ''
+                substituteInPlace setup.py --replace \'setuptools-markdown\' ""
+              '';
+            };
+            pyyaml-include = super.pyyaml-include.overridePythonAttrs {
+              preConfigure = ''
+                substituteInPlace setup.py --replace "setup()" "setup(version=\"1.3\")"
+              '';
+            };
+          })
+        ]);
       in
       rec {
         packages.default = pkgs.poetry2nix.mkPoetryApplication
           {
             projectDir = ./.;
-            overrides = pkgs.poetry2nix.overrides.withDefaults
-              (self: super: {
-                pyparsing = super.pyparsing.overridePythonAttrs (
-                  old: {
-                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
-                  }
-                );
-                jsonschema = super.jsonschema.overridePythonAttrs (
-                  old: {
-                    nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.hatchling self.hatch-vcs ];
-                  }
-                );
-              });
+            inherit overrides;
           };
         apps.default = {
           type = "app";
@@ -32,6 +53,7 @@
         };
         devShell = pkgs.poetry2nix.mkPoetryEnv {
           projectDir = ./.;
+          inherit overrides;
         };
       }
     ));
