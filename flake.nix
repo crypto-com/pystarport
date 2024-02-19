a5d83d5e5a6c05eb7cbcc8f1ac7fa97c8e19f692
yihuang <yi.codeplayer@gmail.com>
2024-02-20 03:22:08 +0800

    Problem: dependency being old
    Solution:
    - relax and upgrade some dependencies
    
    upgrade dotenv
    
    update jsonnet
    
    fix build
    
    upgrade pytest
    
    pytest v8
    
    cleanup

--- flake.nix
+++ flake.nix
@@ -1,45 +1,38 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    poetry2nix = {
+      url = "github:nix-community/poetry2nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
   };
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, poetry2nix }:
     (flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = (import nixpkgs { inherit system; config = { }; });
-        overrides = pkgs.poetry2nix.overrides.withDefaults (pkgs.lib.composeManyExtensions [
-          (self: super:
-            let
-              buildSystems = {
-                eth-bloom = [ "setuptools" ];
-                cprotobuf = [ "setuptools" ];
-                durations = [ "setuptools" ];
-                multitail2 = [ "setuptools" ];
-                pytest-github-actions-annotate-failures = [ "setuptools" ];
-                flake8-black = [ "setuptools" ];
-                multiaddr = [ "setuptools" ];
-              };
-            in
-            pkgs.lib.mapAttrs
-              (attr: systems: super.${attr}.overridePythonAttrs
-                (old: {
-                  nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
-                }))
-              buildSystems
-          )
-          (self: super: {
-            eth-bloom = super.eth-bloom.overridePythonAttrs {
-              preConfigure = ''
-                substituteInPlace setup.py --replace \'setuptools-markdown\' ""
-              '';
+        pkgs = (import nixpkgs {
+          inherit system;
+          config = { };
+          overlays = [
+            poetry2nix.overlays.default
+          ];
+        });
+        overrides = pkgs.poetry2nix.overrides.withDefaults (self: super:
+          let
+            buildSystems = {
+              durations = [ "setuptools" ];
+              multitail2 = [ "setuptools" ];
+              pytest-github-actions-annotate-failures = [ "setuptools" ];
             };
-            pyyaml-include = super.pyyaml-include.overridePythonAttrs {
-              preConfigure = ''
-                substituteInPlace setup.py --replace "setup()" "setup(version=\"1.3\")"
-              '';
-            };
-          })
-        ]);
+          in
+          pkgs.lib.mapAttrs
+            (attr: systems: super.${attr}.overridePythonAttrs
+              (old: {
+                nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ map (a: self.${a}) systems;
+              }))
+            buildSystems
+        );
       in
       rec {
         packages.default = pkgs.poetry2nix.mkPoetryApplication
